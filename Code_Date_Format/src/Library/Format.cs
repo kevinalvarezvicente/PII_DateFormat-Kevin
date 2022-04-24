e96418da2dac1bfb4bb5ef88747e9f92df13572f
kevinalvarezvicente <64856845+kevinalvarezvicente@users.noreply.github.com>
2022-04-24 18:21:15 -0300

    Add files via upload

---
+++ Code_Date_Format/src/Library/Format.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TestDate.src.Library
+{
+    public class Format
+    {
+        public String ChangeFormat1(String year)
+        {
+            return year.Substring(6) + "-" + year.Substring(3, 2) + "-" + year.Substring(0, 2);
+        }
+        public String ChangeFormat2(String year)
+        {
+            return year.Substring(0, 2) + "-" + year.Substring(3, 2) + "-" + year.Substring(6);
+        }
+    }
+}
