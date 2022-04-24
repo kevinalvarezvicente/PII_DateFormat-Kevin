d4f4af11781000aef24e3fe41ab00a1e36a82f50
kevinalvarezvicente <64856845+kevinalvarezvicente@users.noreply.github.com>
2022-04-24 18:23:30 -0300

    Add files via upload

---
+++ Code_Date_Format/src/Program/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using TestDate.src.Library;
+
+namespace TestDate.src.Program
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            string testDate = "10/11/1977";
+            Format FormatClass = new Format();
+            Console.WriteLine("{0} se convierte a: {1} ", testDate, FormatClass.ChangeFormat1(testDate));
+            Console.WriteLine("{0} se convierte a: {1} ", testDate, FormatClass.ChangeFormat2(testDate));
+        }
+    }
+}
